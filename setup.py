620f4c76434acaa47ef0e31bf5848a81d082104b
amiiiinee <mohamedaminebchirg@gmail.com>
2025-12-19 18:49:54 +0100

    add note book trainnig model

--- setup.py
+++ setup.py
@@ -1,19 +1,37 @@
+# from setuptools import setup, find_packages
+
+
+# HYPEN_E_DOT = '-e .'
+# def get_requirements(file_path):
+#     with open(file_path, 'r') as file:
+#         requirements = file.readlines()
+#         requirements = [req.strip() for req in requirements]
+#         if HYPEN_E_DOT in requirements:
+#             requirements.remove(HYPEN_E_DOT)
+#     return requirements
+# setup(
+#     name='ml_project',
+#     version='0.1.0',                         
+#     author='amine',
+#     author_email='mohamedamineg@example.com',
+#     package=find_packages(),
+#     install_requires=get_requirements('requirements.txt'),
+# )
+
 from setuptools import setup, find_packages
 
-
-HYPEN_E_DOT = '-e .'
 def get_requirements(file_path):
+    """Read requirements.txt and return a list of dependencies"""
     with open(file_path, 'r') as file:
         requirements = file.readlines()
-        requirements = [req.strip() for req in requirements]
-        if HYPEN_E_DOT in requirements:
-            requirements.remove(HYPEN_E_DOT)
+        requirements = [req.strip() for req in requirements if req.strip()]
     return requirements
+
 setup(
     name='ml_project',
-    version='0.1.0',                         
+    version='0.1.0',
     author='amine',
     author_email='mohamedamineg@example.com',
-    package=find_packages(),
+    packages=find_packages(),  # FIXED: packages instead of package
     install_requires=get_requirements('requirements.txt'),
 )
--- src/exception.py
+++ src/exception.py
@@ -1,5 +1,5 @@
 import sys
-import logging
+from logger import logging
 import os
 
 logging.basicConfig(
@@ -26,9 +26,9 @@ class CustomException(Exception):
     def __str__(self):
         return self.error_message  
 
-# if __name__ == "__main__":
-#     try:
-#         a = 1 / 0
-#     except Exception as e:
-#         logging.exception("Unhandled exception occurred")
-#         raise CustomException(e, sys)
+if __name__ == "__main__":
+    try:
+        a = 1 / 0
+    except Exception as e:
+        logging.exception("Unhandled exception occurred")
+        raise CustomException(e, sys)
